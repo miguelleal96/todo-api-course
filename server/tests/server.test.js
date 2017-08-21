14c79130e98926ee7888d5621fb90c0bad52c602
miguelleal96 <miguelleal96@gmail.com>
2017-08-21 15:15:32 -0500

    Add test for GET /todos

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -5,10 +5,18 @@ const request = require('supertest')
 const {app} = require('./../server')
 const {Todo} = require('./../models/todo')
 
+const todos = [{
+	text: 'First test todo'
+}, {
+	text: 'Secod test todo'
+}]
+
 /* lifecycle hook (code that runs before each test case) */
 beforeEach(done => {
   /* empties Todo collection */
-  Todo.remove({}).then(() => done())
+  Todo.remove({}).then(() => {
+	  return Todo.insertMany(todos)
+  }).then(() => done()) 
 })
 
 describe('POST /todos', () => {
@@ -29,7 +37,7 @@ describe('POST /todos', () => {
         }
 
         // assert if the todo was successfully added to the DB
-        Todo.find().then((todos) => {
+        Todo.find({text}).then((todos) => {
           expect(todos.length).toBe(1)
           expect(todos[0].text).toBe(text)
           done()
@@ -49,9 +57,21 @@ describe('POST /todos', () => {
 		  
 		  // assert if the todo was not added to the DB
         Todo.find().then((todos) => {
-          expect(todos.length).toBe(0)
+          expect(todos.length).toBe(2)
           done()
         }).catch(e => done(e))
       })
   })
 })
+
+describe('GET /todos', () => [
+	it('should get all todos', (done) => {
+		request(app)
+			.get('/todos')
+			.expect(200)
+			.expect(res => {
+				expect(res.body.todos.length).toBe(2)
+			})
+			.end(done)
+	})
+])
